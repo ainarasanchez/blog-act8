005a3d0017acf0d9b370d76ef3df8073767ec70d
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-06-08 13:45:17 +0200

    Test de rutas

--- src/routes/api/authors.routes.js
+++ src/routes/api/authors.routes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 
-
+// GET /api/authors
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'GET todos los autores (ruta base)' });
+  });
+  
+  // POST /api/authors
+  router.post('/', (req, res) => {
+    res.status(200).json({ message: 'POST crear nuevo autor (ruta base)' });
+  });
 
 module.exports = router
