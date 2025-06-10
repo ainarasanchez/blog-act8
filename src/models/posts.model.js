82bc1040dc8ab74fcc88014bd093deb269c7af5a
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-06-10 20:52:33 +0200

    Corrección del modelo
    Para que cada post recuperado venga con todos los datos del autor

--- src/models/posts.model.js
+++ src/models/posts.model.js
@@ -1,6 +1,6 @@
 const db = require('../config/db');
 const selectAll = async () => {
-    const [result] = await db.query('select * from posts');
+    const [result] = await db.query('SELECT posts.*, authors.name AS autor_nombre, authors.email AS autor_email, authors.image AS autor_imagen FROM posts JOIN authors ON posts.author_id = authors.id');
     return result;
 }
 
