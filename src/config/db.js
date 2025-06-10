9eba6efc49aca61439d65564f52c064e04edca3e
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-06-10 19:49:31 +0200

    Ajustes de conexión

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
 const mysql = require('mysql2');
+require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env;
+
+const pool = mysql.createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_NAME,
 });
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Conectado a MySQL');
-});
-module.exports = connection;
+
+module.exports = pool.promise();
