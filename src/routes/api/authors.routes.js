1f16f6febeba05b58bb76750f000d9701a5e1f7f
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-06-10 22:57:01 +0200

    Nueva ruta dinámica

--- src/routes/api/authors.routes.js
+++ src/routes/api/authors.routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { getAll, create } = require('../../controllers/authors.controller');
-
+const { getByAuthor } = require('../../controllers/posts.controller');
 
 router.get('/', getAll);
+router.get('/:authorId/posts', getByAuthor);
 router.post('/', create);
+
 
 module.exports = router;
